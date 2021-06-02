f64189d9d1d03f3b04249e9435ec6908a8a8f3d8
Fellipe <fellipe_emanoel@univem.edu.br>
2021-06-02 08:48:14 -0300

    First commit

---
+++ src/screens/Dashboard/Dashboard.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+import { Container } from './styles';
+
+export function Dashboard(){
+  return (
+    <Container>
+        <Text>Dashboard</Text>
+    </Container>
+  );
+}
+
+export default Dashboard;
