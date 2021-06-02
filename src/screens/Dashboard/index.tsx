385f1e53ce7bff7229583aacfa8d26fedc4e52e4
Fellipe <fellipe_emanoel@univem.edu.br>
2021-06-02 11:11:59 -0300

    Created header with user infos

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
-import { Container, Title } from './styles';
+
+import { 
+  Container,  
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon
+} from './styles';
+
 
 export function Dashboard(){
   return (
-    <Container>
-        <Title>Dashboard</Title>        
+    <Container>   
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{uri: 'https://avatars.githubusercontent.com/u/48105194?v=4'}}/>
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Fellipe</UserName>
+            </User>
+          </UserInfo>        
+          <Icon name="power"/>
+        </UserWrapper>
+      </Header>           
     </Container>
   );
 }
