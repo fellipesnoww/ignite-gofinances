752ce06463fb2083b7041cdd0312c925834c10dd
Fellipe <fellipe_emanoel@univem.edu.br>
2021-06-07 12:03:00 -0300

    Created a transaction cards

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -9,8 +9,10 @@ import {
   User,
   UserGreeting,
   UserName,
-  Icon
+  Icon,
+  HighlightCards
 } from './styles';
+import { HighlightCard } from '../../components/HighlightCard';
 
 
 export function Dashboard(){
@@ -27,7 +29,27 @@ export function Dashboard(){
           </UserInfo>        
           <Icon name="power"/>
         </UserWrapper>
-      </Header>           
+      </Header>
+      <HighlightCards>
+        <HighlightCard
+          type="up" 
+          title="Entradas" 
+          amount="R$ 17.400,00" 
+          lastTransaction="Última entrada dia 13 de abril"
+        />
+        <HighlightCard 
+          type="down"
+          title="Saídas" 
+          amount="R$ 1.259,00" 
+          lastTransaction="Última saída dia 03 de abril"
+        />
+        <HighlightCard 
+          type="total"
+          title="Total" 
+          amount="R$ 16.141,00" 
+          lastTransaction="01 à 16 de abril"
+        />
+      </HighlightCards>
     </Container>
   );
 }
