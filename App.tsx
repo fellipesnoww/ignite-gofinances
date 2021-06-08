bc159798f7584a69f7b74ba6e911aa01ab162653
Fellipe <fellipe_emanoel@univem.edu.br>
2021-06-08 09:55:13 -0300

    Started a form elements from register

--- App.tsx
+++ App.tsx
@@ -4,6 +4,8 @@ import {ThemeProvider} from 'styled-components';
 
 import theme from './src/global/styles/theme'
 import {Dashboard} from './src/screens/Dashboard';
+import {Register} from './src/screens/Register';
+
 
 import {
   useFonts,
@@ -26,7 +28,7 @@ export default function App() {
   
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <Register />
     </ThemeProvider>
   );
 }
---
+++ src/screens/Register/index.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+
+import { Input } from '../../components/Form/Input';
+import { Button } from '../../components/Form/Button';
+import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton';
+
+import { 
+    Container,
+    Header,
+    Title,
+    Form,
+    Fields,
+    TransactionsType,
+} from './styles';
+
+
+export function Register(){
+    const [transactionType, setTransactionType] = useState('');
+
+    function handleTransactionTypeSelect(type: 'up' | 'down'){
+        setTransactionType(type);
+    }
+
+  return (
+    <Container>
+        <Header>
+            <Title>Cadastro</Title>
+        </Header>
+        <Form>
+            <Fields>
+                <Input placeholder="Nome"/>
+                <Input placeholder="Preço"/>
+                <TransactionsType>
+                    <TransactionTypeButton
+                        title="Income"
+                        type="up"
+                        onPress={() => handleTransactionTypeSelect('up')}
+                        isActive={transactionType === 'up'}
+                    />
+                    <TransactionTypeButton 
+                        title="Outcome"
+                        type="down"
+                        onPress={() => handleTransactionTypeSelect('down')}
+                        isActive={transactionType === 'down'}
+                    />
+                </TransactionsType>
+            </Fields>
+            <Button title="Enviar"/>
+        </Form>
+    </Container>
+    );
+}
