2c826d81e73097169a7be471fd891f5b899d28d7
Fellipe <fellipe_emanoel@univem.edu.br>
2021-06-28 21:07:30 -0300

    Created login screen

--- App.tsx
+++ App.tsx
@@ -16,6 +16,7 @@ import {
 } from '@expo-google-fonts/poppins';
 import AppRoutes from './src/routes/app.routes';
 import { StatusBar, Platform } from 'react-native';
+import { SignIn } from './src/screens/SignIn';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -33,7 +34,8 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <NavigationContainer>
         <StatusBar barStyle="light-content" />
-        <AppRoutes />
+        {/* <AppRoutes /> */}
+        <SignIn />
       </NavigationContainer>
     </ThemeProvider>
   );
