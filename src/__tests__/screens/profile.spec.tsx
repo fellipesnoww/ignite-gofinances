fe6032fd6a176dcf88c882a9c16e2b92f80a0b3f
Fellipe Neves <fellipe2.0e@gmail.com>
2022-11-19 18:36:43 -0300

    Created test category

--- src/__tests__/screens/profile.spec.tsx
+++ src/__tests__/screens/profile.spec.tsx
@@ -4,28 +4,31 @@ import { render } from '@testing-library/react-native';
 
 import Profile from '../../screens/Profile';
 
-test('check if show correctly user input name placeholder', () => {
-    const { getByPlaceholderText } = render(<Profile />); //Renderizou o componente no teste e separou a funcao de consulta por Placeholder
+//Cria uma categoria de teste (um Switch)
+describe('Profile Screen', () => {
+    it('should have placeholder correctly in user input name', () => {
+        const { getByPlaceholderText } = render(<Profile />); //Renderizou o componente no teste e separou a funcao de consulta por Placeholder
+        
+        const inputName = getByPlaceholderText('Nome'); //Realiza a consulta no componente renderizado
     
-    const inputName = getByPlaceholderText('Nome'); //Realiza a consulta no componente renderizado
-
-    expect(inputName).toBeTruthy(); //Cria a expectativa de que um input com placeholder Nome seja encontrado    
-});
-
-test('check if user data has been loaded', () => {
-    const { getByTestId } = render(<Profile />);
-
-    const inputName = getByTestId('input-name');
-    const inputSurname = getByTestId('input-surname');   
-
-    expect(inputName.props.value).toEqual("Fellipe");
-    expect(inputSurname.props.value).toEqual("Neves");
-});
-
-test('check if title render correctly', () => {
-    const { getByTestId } = render(<Profile />);
-
-    const textTitle = getByTestId('text-title');
-
-    expect(textTitle.props.children).toContain('Perfil');
+        expect(inputName).toBeTruthy(); //Cria a expectativa de que um input com placeholder Nome seja encontrado    
+    });
+    
+    it('should be loaded user data', () => {
+        const { getByTestId } = render(<Profile />);
+    
+        const inputName = getByTestId('input-name');
+        const inputSurname = getByTestId('input-surname');   
+    
+        expect(inputName.props.value).toEqual("Fellipe");
+        expect(inputSurname.props.value).toEqual("Neves");
+    });
+    
+    it('should exist title correctly', () => {
+        const { getByTestId } = render(<Profile />);
+    
+        const textTitle = getByTestId('text-title');
+    
+        expect(textTitle.props.children).toContain('Perfil');
+    });
 });
